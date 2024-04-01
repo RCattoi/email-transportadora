cfca25583cb1a9d541b4ef64c73aba8c35bb123a
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-03-31 22:25:04 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+
+const PORT = 3000;
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`It's alive at: ${PORT}`);
+});
+
+app.post('/api/data', (req, res) => {
+  const { to, subject, name, message, reply_to } = req.body.data;
+
+  const USERID = process.env.USERID;
+  emailjs.init(USERID);
+  emailjs
+    .send('service_dzoqy8o', 'template_2akyqfb', {
+      to: to,
+      subject: subject,
+      name: name,
+      message: message,
+      reply_to: reply_to,
+    })
+    .then(function (response) {
+      console.log('Email sent successfully:', response);
+    })
+    .catch(function (error) {
+      console.error('Email sending failed:', error);
+    });
+
+  res.status(200).json({ message: data });
+});
