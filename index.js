65c80c16f0206558a3c12b381cf1c3e1cf6c3faa
Rodrigo Cattoi <rodrigoccattoi@gmail.com>
2024-05-12 00:05:42 -0300

    chore: Update CORS origin to fix transportadora-beta.onrender.com URL

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin: '*',
-    origin: ['https://transportadora-beta.onrender.com/'],
+    origin: ['https://transportadora-beta.onrender.com'],
   })
 );
 
